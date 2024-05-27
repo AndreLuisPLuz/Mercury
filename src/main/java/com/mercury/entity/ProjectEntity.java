0c893f289d5da35960ce057abe165d54b4d8e3be
Nilton Meira <niltonprudys@gmail.com>
2024-05-27 16:51:26 -0300

    feat ProjectEntity.java

---
+++ src/main/java/com/mercury/entity/ProjectEntity.java
@@ -0,0 +1,12 @@
+package com.mercury.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ProjectData")
+
+public class ProjectEntity extends BaseEntity{
+    
+    
+}
