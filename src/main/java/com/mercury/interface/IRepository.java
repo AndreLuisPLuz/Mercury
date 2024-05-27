e4a60cd6531eb8244b1837eb8250b613d5c192d7
AndreLuisPLuz <andreluispluz@gmail.com>
2024-05-27 16:56:11 -0300

    feat: added IRepository interface.

---
+++ src/main/java/com/mercury/interface/IRepository.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+
+import com.mercury.entity.BaseEntity;
+
+public interface IRepository<T extends BaseEntity> {
+    CompletableFuture<T> create(T entity);
+    CompletableFuture<T> update(T entity);
+    CompletableFuture<Boolean> delete(Long id);
+    CompletableFuture<Boolean> delete(T entity);
+    CompletableFuture<ArrayList<T>> selectMany();
+    CompletableFuture<T> select();
+}
