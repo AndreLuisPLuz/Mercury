1232afa5e1dbc400043da3e78f98614ae279e83f
AndreLuisPLuz <andreluispluz@gmail.com>
2024-05-27 16:14:23 -0300

    feat: BaseEntity class added

---
+++ src/main/java/com/mercury/entity/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.mercury.entity;
+
+import javax.persitance;
+import java.util.Date;
+import java.time.*;
+
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column(name = "createdAt")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+    
+    @Column(name = "updatedAt")
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
+
+    public BaseEntity() {
+        createdAt = new Date();
+        updatedAt = new Date();
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
