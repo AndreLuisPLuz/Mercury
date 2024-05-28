d7df4a09047799949cffb43dfea9f015cc8811a6
MuryloSaladino <murylosaladino@gmail.com>
2024-05-28 16:18:41 -0300

    feat: login image

--- src/main/java/com/mercury/controllers/HomeSceneController.java
+++ src/main/java/com/mercury/controllers/HomeSceneController.java
@@ -1,18 +1,7 @@
 package com.mercury.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class HomeSceneController {
     
-    protected String test;
-
-    @FXML
-    protected Button btnTest;
-
-    @FXML
-    protected void initializeEvent(ActionEvent e) throws Exception {
-        btnTest.setText(test);
-    }
+    
 }
