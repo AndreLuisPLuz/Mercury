66d6e2c96a583175af02b25249d54406fb418317
AndreLuisPLuz <andreluispluz@gmail.com>
2024-05-28 13:46:28 -0300

    feat: baserepository

--- src/main/java/com/mercury/App.java
+++ src/main/java/com/mercury/App.java
@@ -1,8 +1,10 @@
 package com.mercury;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import com.mercury.enums.Page;
+
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -11,10 +13,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Scene scene = LoginSceneController.CreateScene();
-        // primaryStage.setScene(scene);
-        // primaryStage.show();
-
-        System.out.println("Maven cretino");
+        primaryStage.setScene(Page.LOGIN.loadScene());
+        primaryStage.show();
     }
+
 }
---
+++ src/main/java/com/mercury/enums/Page.java
@@ -0,0 +1,54 @@
+package com.mercury.enums;
+
+import com.mercury.App;
+
+import java.net.URL;
+import java.util.function.Consumer;
+
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+public enum Page {
+    LOGIN("login-scene.fxml"),
+    HOME("home-scene.fxml"),
+    PROJECT("project-scene.fxml");
+
+    private String filepath;
+
+    
+    public Scene loadScene() {
+        URL url = App.class.getResource(filepath);
+        FXMLLoader loader = new FXMLLoader(url);
+
+        try {
+            return new Scene(loader.load());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public <T> Scene loadScene(Consumer<T> consumer) {
+        URL url = App.class.getResource(filepath);
+        FXMLLoader loader = new FXMLLoader(url);
+        Scene scene;
+        
+        try {
+            scene = new Scene(loader.load());
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        T controller = loader.getController();
+        consumer.accept(controller);
+
+        return scene;
+    }
+
+    Page(String path) {
+        this.filepath = path;
+    }
+}
