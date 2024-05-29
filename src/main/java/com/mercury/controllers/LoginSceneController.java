dfdced16d73a9ac6b42a5b89d2dac1697570f34d
AndreLuisPLuz <andreluispluz@gmail.com>
2024-05-29 15:25:50 -0300

    feat: partial loginscenecontroller

--- src/main/java/com/mercury/controllers/LoginSceneController.java
+++ src/main/java/com/mercury/controllers/LoginSceneController.java
@@ -1,6 +1,8 @@
 package com.mercury.controllers;
 
 import com.mercury.enums.Page;
+import com.mercury.repository.Repository;
+import com.mercury.services.UserService;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginSceneController {
+    private final UserService uService = new UserService();
     
     @FXML
     private TextField tfUsername;
@@ -25,5 +28,26 @@ public class LoginSceneController {
         // Como trocar de tela:
         Stage stage = (Stage)btLogin.getScene().getWindow();
         stage.setScene(Page.PROJECT.loadScene());
+    }
+
+    @FXML
+    private void btLoginClick(ActionEvent e) {
+        String username = tfUsername.getText();
+        String password = tfPassword.getText();
+
+        Boolean isValidLogin;
+
+        try {
+            isValidLogin = uService.isLoginAttemptValid(username, password).get();
+        } catch (Exception ex) {
+            isValidLogin = false;
+        }
+
+        if (isValidLogin) {
+            Stage stage = (Stage)btLogin.getScene().getWindow();
+            stage.setScene(Page.HOME.loadScene((HomeSceneController e) -> {
+                e.
+            }));
+        }
     }
 }
