a902b3456dc072733545f47879bc5ac4ed39fbe8
NiltonMeira <niltonprudys@gmail@gmail.com>
2024-06-03 16:49:51 -0300

    feat: HomeSceneController

--- src/main/java/com/mercury/App.java
+++ src/main/java/com/mercury/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(Page.LOGIN.loadScene());
+        primaryStage.setScene(Page.HOME.loadScene());
         primaryStage.show();
     }
 
--- src/main/java/com/mercury/controllers/HomeSceneController.java
+++ src/main/java/com/mercury/controllers/HomeSceneController.java
@@ -6,14 +6,32 @@ import java.util.List;
 import com.mercury.entity.ProjectEntity;
 import com.mercury.entity.UserEntity;
 import com.mercury.repository.Repository;
+import com.microsoft.sqlserver.jdbc.dataclassification.Label;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;;
 
 public class HomeSceneController {
     
     protected UserEntity user;
     protected Repository<ProjectEntity> repository = new Repository(ProjectEntity.class);
     protected List<ProjectEntity> projects;
+    protected String labelContent = "Pimba";
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private Pane panezinho;
+
+    @FXML
+    private FlowPane flowPane;
+
+
+
 
     @FXML
     protected void initialize()
@@ -25,9 +43,19 @@ public class HomeSceneController {
             System.out.println("Você foi pimbado");
         }
 
-        BorderPane pane = new BorderPane();
-        
+        Pane pane = new Pane();
+        pane.setBorder(new Border(new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        pane.setPrefSize(200, 200);
+        Button btn = new Button();
 
+       Text text =  new Text(labelContent);
+       text.setFill(Color.WHITE);
+
+        pane.getChildren().add(text); 
+        text.setLayoutX(50);
+        text.setLayoutY(50);
+        pane.getChildren().addAll(btn);  
+        flowPane.getChildren().add(pane);
 
         
     }
